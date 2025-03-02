9e2f5bd1890e26506492c68bcb830bca499ab863
belloLucas <lucasbellodev@gmail.com>
2025-03-02 15:34:29 -0300

    feat: utilizing TypOrm on user entity

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [UserController],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
 })
 export class UserModule {}
