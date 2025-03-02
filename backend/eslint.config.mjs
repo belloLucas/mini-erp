1f1acbfa24c22ecf07e0f03127682b7d60d50968
belloLucas <lucasbellodev@gmail.com>
2025-03-02 15:30:06 -0300

    feat: updating lint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -31,6 +31,8 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      'function-parentheses-newline': 'off',
+      'function-paren-newline': 'off',
     },
   },
 );
