fe75a8d90ddec86d7bef5270a05c5a0c2bfc9b04
belloLucas <lucasbellodev@gmail.com>
2025-03-04 16:20:13 -0300

    feat: exporting UserService to use on otherplaces

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -8,5 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   controllers: [UserController],
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
